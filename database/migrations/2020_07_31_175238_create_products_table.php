3b484408ef22de01b067485e8aa587cd85ffcae2
Josemprog <josek-1@hotmail.com>
2020-07-31 12:57:11 -0500

    feat:The command (php artisan make:model) was used with the flags (-m, -s and -r) to create the model, the migration, the factory and the seeder for the products.

---
+++ database/migrations/2020_07_31_175238_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand');
+            $table->string('name');
+            $table->double('unit_price');
+            $table->bigInteger('quantity');
+            $table->string('description');
+            $table->string('image');
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
